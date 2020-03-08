4e8a49c95e5bd7856b7ae9aa5a83a4b989f11d18
Seremba <pserembae@gmail.com>
2020-03-08 10:21:32 -0700

    create portfolio website

---
+++ js/index.js
@@ -0,0 +1,17 @@
+const navToggle = document.querySelector('.nav-toggle');
+const navLinks = document.querySelectorAll('.nav__link')
+
+navToggle.addEventListener('click', () => {
+    document.body.classList.toggle('nav-open');
+});
+
+navLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        document.body.classList.remove('nav-open');
+    })
+})
+
+
+AOS.init({
+    duration: 1500,
+  })
